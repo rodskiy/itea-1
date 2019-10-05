dab9d2707afc2bce79b8010e3e4e5aa17272ddcc
Pavlo Rodskiy <rodskiy@ukr.net>
2019-10-05 22:00:45 +0300

    Modified entrypoint.sh

--- Dockerfiles/For_nginx/entrypoint.sh
+++ Dockerfiles/For_nginx/entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "<head></head><body><h1 align=center>HELLO $NAME!</h1></body>" > index.htm
+echo "This is entrypoint.sh" 
+echo "Create new index.html" 
+echo "<head></head><body><body><h1 align=center>Hello $NAME</h1></body>" > /usr/share/nginx/html/index.html
+/usr/sbin/nginx -g "daemon off;" 
+
